ad7ccaed9342a0466a5bcc515346666f2beb633e
Alugaí Web <156407297+alugaiweb@users.noreply.github.com>
2024-01-14 06:40:51 -0300

    nav-bar

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+function showMenuMobile() {
+    let menuMobileOpen = document.querySelector('.menu-mobile');
+    menuMobileOpen.classList.add('open-menu-mobile')
+}
+
+function closedMenuMobile() {
+    let menuMobileClosed = document.querySelector('.menu-mobile');
+    menuMobileClosed.classList.remove('open-menu-mobile')
+}
+
+
+
